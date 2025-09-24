cefbd02803a45e77a92140eae2318113d84106a4
vadimborovik <v.borovuk@softum.io>
2025-09-24 11:15:52 +0300

    change express static

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,10 +29,6 @@ app.use('/api/blogs', blogRoutes)
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
 }
 
 app.listen(PORT,()=>{
